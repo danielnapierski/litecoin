0ecd7ae7079cf8177a4a86491bfb0f0a9cd38e4e
Daniel Napierski <danielpnapierski@gmail.com>
2017-08-29 11:29:10 -0700

    Initial Pawscoin changes.

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -260,11 +260,11 @@ UniValue stop(const JSONRPCRequest& jsonRequest)
     if (jsonRequest.fHelp || jsonRequest.params.size() > 1)
         throw runtime_error(
             "stop\n"
-            "\nStop Litecoin server.");
+            "\nStop Pawscoin server.");
     // Event loop will exit after current HTTP requests have been handled, so
     // this reply will get back to the client.
     StartShutdown();
-    return "Litecoin server stopping";
+    return "Pawscoin server stopping";
 }
 
 /**
@@ -518,7 +518,7 @@ std::string HelpExampleCli(const std::string& methodname, const std::string& arg
 std::string HelpExampleRpc(const std::string& methodname, const std::string& args)
 {
     return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
-        "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:9332/\n";
+        "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:34343/\n";
 }
 
 void RPCSetTimerInterfaceIfUnset(RPCTimerInterface *iface)
